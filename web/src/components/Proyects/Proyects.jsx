b3ce55d8152ad4702d4a173e9b82757dd41b9e71
Fran <fransanxenxo@gmail.com>
2022-10-04 15:16:42 +0100

    feat: nuevo proyecto añadidod

--- web/src/components/Proyects/Proyects.jsx
+++ web/src/components/Proyects/Proyects.jsx
@@ -1,6 +1,7 @@
 import './Proyects.css'
 import bike_locker from '../../assets/images/bike-locker.png'
 import rallye from '../../assets/images/placa.png'
+import jorunal from '../../assets/images/journal-app.png'
 import mern from '../../assets/images/MERN.png'
 import postman from '../../assets/images/postman.png'
 import bootstrap from '../../assets/images/bootstrap-social-logo.png'
@@ -57,10 +58,18 @@ function Proyects() {
                             </div>
                             <div className='card-link-portfolio'>
                                 <button className='btn-git'>
-                                    <a href="https://github.com/franlopezhack/SHOP-BIKES" className='btn-git-a' target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'white' }}>Repo GitHub</a>
+                                    <a href="https://github.com/franlopezhack/Journal-app"
+                                        className='btn-git-a' target="_blank" rel="noreferrer"
+                                        style={{ textDecoration: 'none', color: 'white' }}>
+                                        Repo GitHub
+                                    </a>
                                 </button>
                                 <button className='btn-git'>
-                                    <a href="https://iron-bike-shop.herokuapp.com/" className='btn-git-a' target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'white' }}>Link</a>
+                                    <a href="https://journal-fran.netlify.app/"
+                                        className='btn-git-a' target="_blank" rel="noreferrer"
+                                        style={{ textDecoration: 'none', color: 'white' }}>
+                                        Link
+                                    </a>
                                 </button>
                             </div>
                         </div>
@@ -112,6 +121,30 @@ function Proyects() {
                                 <button className='btn-git'>
                                     <a href="https://www.rallyedepontevedra.com/" target="_blank" rel="noreferrer" className='btn-git-a' style={{ textDecoration: 'none', color: 'white' }}>Link</a>
                                 </button>
+                            </div>
+                        </div>
+                        <div className='card_div'>
+                            <p> JOURNAL APP</p>
+                            <div className='card'>
+                                <div className='img-card'>
+                                    <img src={jorunal} alt={jorunal} />
+                                </div>
+
+                                <hr />
+
+                                <div className='card-body'>
+                                    {/* <img src={mern} alt={mern} />
+                                <img src={postman} alt={postman} />
+                                <img id='bs' src={bootstrap} alt={bootstrap} /> */}
+                                </div>
+                                <div className='card-link'>
+                                    <button className='btn-git'>
+                                        <a href="https://github.com/franlopezhack/rallye-pontevedra" className='btn-git-a' target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'white' }}>Repo GitHub</a>
+                                    </button>
+                                    <button className='btn-git'>
+                                        <a href="https://www.rallyedepontevedra.com/" target="_blank" rel="noreferrer" className='btn-git-a' style={{ textDecoration: 'none', color: 'white' }}>Link</a>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
