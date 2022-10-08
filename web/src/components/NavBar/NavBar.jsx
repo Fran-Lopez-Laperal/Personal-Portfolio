dfbb4365de39440e62ac732f5c5060c541c5c8db
Fran <fransanxenxo@gmail.com>
2022-10-08 17:20:54 +0100

    cambios en nav-bar

--- web/src/components/NavBar/NavBar.jsx
+++ web/src/components/NavBar/NavBar.jsx
@@ -9,11 +9,6 @@ function NavBar() {
             <div className="nav-bar">
 
                 <div className='nav-container'>
-                    <Link to='home'>
-                        <p className='letters'>FLL  💻</p>
-                    </Link>
-                    <input type="checkbox" id="menu-bar" />
-                    <label className="fa fa-bars" htmlFor="menu-bar"></label>
                     <nav className='menu'>
                         <Link style={{ fontSize: '40px', fontFamily: 'Courier, monospace', textDecoration: 'underline', color: 'rgb(3, 189, 154)' }} to='about-me'>Sobre Mí</Link>
                         <Link style={{ fontSize: '40px', fontFamily: 'Courier, monospace', textDecoration: 'underline' }} to='proyects'><small>Mis proyectos</small> </Link>
