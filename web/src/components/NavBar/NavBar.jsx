a548902c605739c64f8141a50ac5440965030149
Fran <fransanxenxo@gmail.com>
2022-10-11 19:55:32 +0100

    feat:change nav-bar color

--- web/src/components/NavBar/NavBar.jsx
+++ web/src/components/NavBar/NavBar.jsx
@@ -7,15 +7,26 @@ function NavBar() {
     return (
         <>
             <div className="nav-bar">
-
                 <div className='nav-container'>
                     <nav className='menu'>
-                        <Link className="fa fa-home" aria-hidden="true" to='home'></Link>
-                        <Link className='fa fa-user' to='about-me' ></Link>
-                        <Link className="fa fa-laptop"  aria-hidden="true"></Link>
-                        <Link className="fa fa-code-fork" aria-hidden="true" to=''></Link>
-                        <Link className="fa fa-briefcase" aria-hidden="true" to='proyects'></Link>
-                        <Link className="fa fa-phone" aria-hidden="true" to='email'></Link>
+                        <div className='inner'>
+                            <Link className="fa fa-home" aria-hidden="true" to='home'></Link>
+                        </div>
+                        <div className='inner'>
+                            <Link className='fa fa-user' to='about-me' ></Link>
+                        </div>
+                        <div className='inner'>
+                            <Link className="fa fa-laptop" aria-hidden="true"></Link>
+                        </div>
+                        <div className='inner'>
+                            <Link className="fa fa-code-fork" aria-hidden="true" to=''></Link>
+                        </div>
+                        <div className='inner'>
+                            <Link className="fa fa-briefcase" aria-hidden="true" to='proyects'></Link>
+                        </div>
+                        <div className='inner'>
+                            <Link className="fa fa-phone" aria-hidden="true" to='email'></Link>
+                        </div>
                     </nav>
                 </div>
             </div>
