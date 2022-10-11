9e331b50e85c42308a0970787bbceaca37dc5bb6
Fran <fransanxenxo@gmail.com>
2022-10-11 16:17:08 +0100

    feat: new nav-bar

--- web/src/components/NavBar/NavBar.jsx
+++ web/src/components/NavBar/NavBar.jsx
@@ -10,12 +10,12 @@ function NavBar() {
 
                 <div className='nav-container'>
                     <nav className='menu'>
-                        <Link className="fa fa-home" aria-hidden="true" style={{ fontSize: '40px' }} to='home'></Link>
-                        <Link className='fa fa-user' style={{ fontSize: '40px' }} to='about-me' ></Link>
-                        <Link className="fa fa-laptop"  aria-hidden="true" style={{ fontSize: '40px' }}></Link>
-                        <Link className="fa fa-code-fork" aria-hidden="true" style={{ fontSize: '40px' }} to=''></Link>
-                        <Link className="fa fa-briefcase" aria-hidden="true" style={{ fontSize: '40px' }} to='proyects'></Link>
-                        <Link className="fa fa-phone" aria-hidden="true" style={{ fontSize: '40px' }} to='email'></Link>
+                        <Link className="fa fa-home" aria-hidden="true" to='home'></Link>
+                        <Link className='fa fa-user' to='about-me' ></Link>
+                        <Link className="fa fa-laptop"  aria-hidden="true"></Link>
+                        <Link className="fa fa-code-fork" aria-hidden="true" to=''></Link>
+                        <Link className="fa fa-briefcase" aria-hidden="true" to='proyects'></Link>
+                        <Link className="fa fa-phone" aria-hidden="true" to='email'></Link>
                     </nav>
                 </div>
             </div>
